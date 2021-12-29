14a9904be4e3aea3f4416f3231db41df2e638f92
RalfiWardhana <ralfigeophysics@gmail.com>
2021-12-29 21:14:14 +0700

    deploy_server3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 const route = require("./src/router/index")
-const port = 2022;
+const port = process.env.PORT || 2022;
 
 app.use(cors())
 app.use(express.json())
