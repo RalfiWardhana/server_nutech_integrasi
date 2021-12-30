75ba30edebc707164daa22a03de31dbbeae74e47
Ralfi Wardhana <78859941+RalfiWardhana@users.noreply.github.com>
2021-12-30 14:19:55 +0700

    Update barang.js

--- src/controllers/barang.js
+++ src/controllers/barang.js
@@ -42,7 +42,8 @@ exports.getBarangs =async(req,res) => {
             attributes : {
                 exclude:["createdAt","updatedAt"]
             }      
-        }
+        },
+         order: [['updatedAt', 'DESC']]  
          
            
        })
